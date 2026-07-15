e72a67f9ac83528b83df5cdc69f4740fd2ecc897
Your Name <you@example.com>
2026-07-15 22:32:01 +0530

    Initial commit: Spring Boot REST Service project

---
+++ src/main/java/com/prasadJayant/controller/GreetingController.java
@@ -0,0 +1,27 @@
+package com.prasadJayant.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class GreetingController {
+	final Greeting greeting;
+	AtomicLong counter = new AtomicLong();
+
+	GreetingController(Greeting greeting) {
+		this.greeting = greeting;
+	}
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name") String name) {
+		greeting.setContent("Hey I am learning Spring Boot " + name);
+		greeting.setId(counter.incrementAndGet());
+		return greeting;
+
+	}
+
+}
